d714d734bcf19d5a111217e4a451cd7c7550cc5d
div-yangana <0.0divyangana0.0@gmail.com>
2023-05-20 20:24:31 +0530

    First Commt

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,8 +1,143 @@
-import React from "react";
+import React,{useState} from "react";
 import './Portal.css'
+import UploadCard from '../UploadCard/UploadCard'
+import logo from '../../images/company_LOGO_2.svg'
+import user from '../../images/user.svg'
+import imgfileUpload from '../../images/file.svg'
+import imgfileDownload from'../../images/file_download.svg'
+import deleteLogo from '../../images/deleteLogo.svg'
+
 const Portal = () => {
+
+  const[modal,setModal]=useState(false);
+  const [fileDetails, setFileDetails] = useState([]);
+  const [arr,setArr]=useState([]);
+  const [showResults, setShowResults] = useState(false); 
+  const [buttonColor, setbuttonColor] = useState('#989898'); 
+  const [generate,setGenrate]=useState('Generate')
+  let value =0;
+
+  const toggleModal=()=>{
+    setModal(!modal)
+  }
+
+  const handleClick=()=>{
+    if(fileDetails){
+      setArr(fileDetails)
+      setShowResults(true);
+      setbuttonColor('#2F2CAB')
+      setGenrate('Finish')
+      ++value
+      if(value==2){
+        window.location.reload();
+      }
+    }
+  }
+
   return (
-    <div>Portal</div>
+    
+    <>  
+
+        {!modal&&(
+            <div className="outercontainer"> 
+            <div className="navbar">
+                <img src={logo} id="logo"></img>
+                <img src={user} id="user"></img> 
+            </div>
+  
+            <div className="userDetails">
+              <div className="Hello">
+                HELLO!
+              </div>
+              <div className="User">
+                USER USER
+              </div>
+            </div>
+  
+            <div className="BottomContainer">
+
+
+                <div className={`documents ${fileDetails? 'classA': 'classB'}`}>
+                    <div className="docHeading">
+                      Generate Test Cases 
+                    </div>
+                    <div className="upload">
+                        
+                        <div className="beforeModal" onClick={toggleModal}>
+                          {<img src={imgfileUpload} id="uploadFile"/>}
+                          <div className="uploadText">
+                            Upload User Story Documents
+                          </div>
+                        </div>
+                        
+                        {fileDetails && fileDetails.map((file)=>{
+                          
+                          return <div className="fileUploadCard">
+                          <div className="wrapperDiv">
+                            <img src={imgfileUpload} id="file"></img>
+                            <div className="fileDetails">
+                                <div className="fileName">
+                                  {file.name}
+                                </div>
+                                <div className="fileSize">
+                                  {file.size} kb
+                                </div>
+                            </div>
+                          </div>
+                          
+                          <img src={deleteLogo}></img>   
+                        </div>
+                        })}
+                        
+                      </div>
+                    </div>
+
+                  
+            <div className="results">
+                    {showResults&&(<div className="resultsText">
+                      Results
+                      </div>) }
+
+            {arr && arr.map((file) =><div className="fileResultCard">
+                      <div className="wrapperDiv">
+                        <img src={imgfileUpload} id="file"></img>
+                            <div className="fileDetails">
+                                <div className="fileName">
+                                  TestCases.pdf
+                                </div>
+                                <div className="fileSize">
+                                  {file.size} kB
+                                </div>
+                            </div>
+                      </div>    
+                      <img src={imgfileDownload} id="downloadFile"></img>   
+                  </div>)}
+                </div>
+
+
+                <div className="finish">
+                  
+                  <button onClick={handleClick} style={{ backgroundColor: buttonColor} }id="generate">
+                    {generate}
+                  </button>
+                </div>
+
+                </div>   
+                 
+                
+
+            </div>
+          
+        )}
+        
+        
+        {modal&&(
+          <UploadCard setFileDetails={setFileDetails} modal={modal} setModal={setModal} onClickFunction={toggleModal} status={modal} />
+        )} 
+                
+        
+        {/* </div>   */}
+  </>
   )
 }
 
--- src/components/UploadCard/UploadCard.js
+++ src/components/UploadCard/UploadCard.js
@@ -1,28 +1,95 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useState } from "react";
 import './UploadCard.css';
 import fileImage from '../../images/file.svg'
 
-const UploadCard = () => {
+const UploadCard = ({ setModal, setFileDetails}) => {
+  
+  const [files,setFiles]=useState(null);
+//   var obj={};
+  const inputRef=useRef();
+  const handleDragOver=(e)=>{
+    e.preventDefault();
+  }
+  const handleDrop=(e)=>{
+    e.preventDefault();
+    console.log(e.dataTransfer.files[0].name);
+    setFiles(e.dataTransfer.files);
+  }
+
+
+
+  const handleSubmit= ()=>{
+    if(files){
+        Array.from(files).map((file)=>{
+            let obj = {
+                name: file.name,
+                size: file.size
+            };
+            setFileDetails((prevValues)=> [...prevValues, obj])
+        })
+    }
+    setModal(false);
+  }
+
+
   return (
     <>
+    
      <div className="parentContainer">
         <div className="childContainer">
             <div className="heading">
                 Upload user story documents
             </div>
-            <div className="uploadArea">
-                <div className="uploadIcon">
-                    <img src={fileImage} id="fileImage"></img>
+            <div className="rest">
+
+            
+            {!files&&(
+
+                <label for="addFile">
+                <div className="uploadArea"
+                onDragOver={handleDragOver}
+                onDrop={handleDrop} 
+                >
+                    <div className="uploadIcon">
+                        <img src={fileImage} id="fileImage"></img>
+                    </div>
+                    <div className="uploadMessage">
+                        
+                            <div>Tap to choose files</div>
+                            <div>from your device</div>
+                        
+                        <input type="file" 
+                        multiple name=" " id="addFile"
+                        onChange={(e)=>setFiles(e.target.files)}
+                        ref={inputRef}
+                        />
+                        
+                    </div>
+
                 </div>
-                <div className="uploadMessage">
-                    <div>Tap to choose files</div>
-                     <div>from your device</div>
-                </div>
+                </label>  
+            )}
+            {files&&(
+                <ul >
+                    {Array.from(files).map((file,index)=> <li key={index}>{file.name}</li>)}
+                    {/* {
+                        filesDetails=Array.from(files).map((file,index)=>{
+                            //  obj={
+                            //     fileName: file.name,
+                            //     fileSize:file.size,
+                            // };return obj;
+                            filesDetails.push(files.name);
+                        })} */}
+                    {console.log(files)}
+                </ul>       
+            )}
 
             </div>
+            
             <div className="buttons">
-                <button id="cancel">Cancel</button>
-                <button id="upload">Upload</button>
+                <button onClick={()=> setModal(false)} id="cancel" >Cancel</button>
+                <button onClick={handleSubmit} id="upload">Upload</button>
             </div>
             
         </div>
